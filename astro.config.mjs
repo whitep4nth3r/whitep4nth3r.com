607553c91b0c4196df5b0fa9c11a6fa50d5f137b
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2023-12-08 14:52:24 +0000

    Reconfigure some scss usage

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,29 @@ import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
 
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://whitep4nth3r.com",
   integrations: [sitemap()],
+  vite: {
+    resolve: {
+      alias: {
+        "@/": `${path.resolve(__dirname, "src")}/`,
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // path to your scss variables
+          additionalData: `@import "@/styles/_global.scss";`,
+        },
+      },
+    },
+  },
 });
