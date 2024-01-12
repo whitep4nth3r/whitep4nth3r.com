8590a49459cac7e9180f88fbe90ea683ffe4e85e
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2024-01-12 15:02:46 +0000

    Blog post still in progress, just rich text to do

---
+++ src/lib/tools.js
@@ -0,0 +1,35 @@
+export const Tools = {
+  slugifyString: (string) => {
+    return string
+      .replace(/\s+/g, "-")
+      .replace(/[^\w\-]+/g, "")
+      .replace(/\-\-+/g, "-")
+      .replace(/^-+/, "")
+      .replace(/-+$/, "")
+      .toLowerCase();
+  },
+  structuredPostData: ({ post, imageUrl }) => {
+    return JSON.stringify({
+      "@context": "https://schema.org/",
+      "@type": "BlogPosting",
+      mainEntityOfPage: {
+        "@type": "WebPage",
+        "@id": `https://whitep4nth3r.com/blog/${post.slug}`,
+      },
+      headline: post.title,
+      image: [imageUrl],
+      dateCreated: post.date,
+      description: post.excerpt,
+      keywords: post.topicsCollection.items
+        .map((topic) => topic.name)
+        .join(","),
+      author: {
+        "@type": "Person",
+        name: post.author.name,
+        description: post.author.bioShort,
+        image: post.author.imageBio.url,
+        url: "https://whitep4nth3r.com",
+      },
+    });
+  },
+};
